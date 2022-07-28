c403cbc97fa66eb48cd31c70f505b6ffab7c9a86
Roman <rombom24@gmail.com>
2022-07-28 23:38:43 +0300

    start-repository

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', function(){
+	
+
+	let menu = document.querySelector('.menu');
+	let posTop = window.pageYOffset;
+	if (posTop>892){ 
+	alert(posTop) 
+	}
+
+	menu.addEventListener('click', function(e){
+		let link = e.target;
+		// let posTop = window.pageYOffset;
+		// console.log(posTop)
+
+		if(link.classList.contains('menu__link')){
+			e.preventDefault();
+
+			menu.querySelector('.menu__link-active').classList.remove('menu__link-active');
+			link.classList.add('menu__link-active');
+			scrollToTarget(link.hash);
+		}
+	});
+
+	if(location.hash !== ''){
+		scrollToTarget(location.hash);
+	}
+
+	function scrollToTarget(id){
+		let target = document.querySelector(id);
+
+		if(target !== null){
+			let pos = target.offsetTop - 70;
+
+			window.scrollTo({
+				top: pos,
+				behavior: 'smooth'
+			});
+
+			/* window.scrollTo(0, pos); */
+		}
+	}
+
+});
